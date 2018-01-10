8ede43ff5f7973fe3da475d59947aa731124a6a6
Adrian Irving-Beer <wisq@wisq.net>
2018-01-10 10:09:48 -0500

    Add exprotobuf.

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule SpaceEx.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:exprotobuf, "~> 1.2.9"},
     ]
   end
 end
