96e337aeab4fdab225dff0ec26fbe080cec7687e
Adrian Irving-Beer <wisq@wisq.net>
2018-01-13 04:07:31 -0500

    Rework to try special decoders first.

--- lib/space_ex/types.ex
+++ lib/space_ex/types.ex
@@ -6,58 +6,102 @@ defmodule SpaceEx.Types do
   end
 
   defmacro decode(input, {:%{}, _, tuples}) do
-    map = Map.new(tuples)
-    decode_ast(input, map)
+    opts = Map.new(tuples)
+    code = Map.fetch!(opts, "code") |> String.to_atom
+    types = Map.get(opts, "types", nil)
+
+    decoder = type_decoder(input, code, opts)
+
+    if post = processor(:post_decode, code, types, opts) do
+      {:"|>", [], [decoder, post]}
+    else
+      decoder
+    end
   end
 
-  def decode_ast(input, %{"code" => "LIST", "types" => [subtype]}) do
-    quote location: :keep do
-      SpaceEx.Protobufs.List.decode(unquote(input)).items
+  def raw_decoder(input, module, example) do
+    <<first_byte, _ :: binary>> =
+      module.new(value: example)
+      |> module.encode
+
+    quote do
+      <<unquote(first_byte)>> <> unquote(input)
+      |> unquote(module).decode
+      |> Map.fetch!(:value)
+    end
+  end
+
+  def type_decoder(input, :BYTES, _opts),  do: input
+  # TODO: maybe render classes as structs?
+  def type_decoder(input, :CLASS, _opts),  do: input
+
+  def type_decoder(input, :STRING, _opts), do: raw_decoder(input, Raw.String, "dummy")
+    # Strings can also be decoded via:
+    #quote do
+    #  {size, str} = :gpb.decode_varint(unquote(input))
+    #  <<_str :: bytes-size(size)>> = str
+    #end
+  def type_decoder(input, :FLOAT, _opts),  do: raw_decoder(input, Raw.Float, 1.23)
+  def type_decoder(input, :DOUBLE, _opts), do: raw_decoder(input, Raw.Double, 1.23)
+  def type_decoder(input, :SINT32, _opts), do: raw_decoder(input, Raw.SInt32, 123)
+
+  def type_decoder(input, :BOOL, _opts) do
+    quote do
+      case unquote(input) do
+        <<0>> -> false
+        <<1>> -> true
+      end
+    end
+  end
+
+  def type_decoder(input, type, _opts) do
+    if module = protobuf_module(type) do
+      quote do: unquote(module).decode(unquote(input))
+    else
+      raise "No Protobuf module found for type #{type}"
+    end
+  end
+
+  def processor(:post_decode, :LIST, subtypes, _opts) do
+    [subtype] = subtypes
+    quote do
+      Map.fetch!(:items)
       |> Enum.map(fn item ->
         SpaceEx.Types.decode(item, unquote(subtype))
       end)
     end
   end
 
-  def decode_ast(input, %{"code" => "SET", "types" => [subtype]}) do
-    quote location: :keep do
-      SpaceEx.Protobufs.Set.decode(unquote(input)).items
+  def processor(:post_decode, :SET, subtypes, _opts) do
+    [subtype] = subtypes
+    quote do
+      Map.fetch!(:items)
       |> MapSet.new(fn item ->
         SpaceEx.Types.decode(item, unquote(subtype))
       end)
     end
   end
 
-  def decode_ast(input, %{"code" => "TUPLE", "types" => subtypes}) do
-    variables =
-      Enum.map(?a..?z, fn n ->
-        <<n>>
-        |> String.to_atom
-        |> Macro.var(__MODULE__)
-      end)
-      |> Enum.take(Enum.count(subtypes))
-
-    decoders =
-      Enum.zip(subtypes, variables)
-      |> Enum.map(fn {subtype, var} ->
-        quote do
-          unquote(var) = SpaceEx.Types.decode(unquote(var), unquote(subtype))
+  def processor(:post_decode, :TUPLE, subtypes, _opts) do
+    decoders = Enum.map(subtypes, fn subtype ->
+      quote do
+        fn item ->
+          SpaceEx.Types.decode(item, unquote(subtype))
         end
-      end)
+      end
+    end)
 
-    ast_before = quote do
-      unquote(variables) = SpaceEx.Protobufs.Tuple.decode(unquote(input)).items
+    quote do
+      Map.fetch!(:items)
+      |> SpaceEx.Types.decode_tuple(unquote(decoders))
     end
-    ast_after = quote do
-      {unquote_splicing(variables)}
-    end
-
-    {:__block__, [], [ast_before] ++ decoders ++ [ast_after]}
   end
 
-  def decode_ast(input, %{"code" => "DICTIONARY", "types" => [key_type, value_type]}) do
-    quote location: :keep do
-      SpaceEx.Protobufs.Dictionary.decode(unquote(input)).entries
+  def processor(:post_decode, :DICTIONARY, subtypes, _opts) do
+    [key_type, value_type] = subtypes
+
+    quote do
+      Map.fetch!(:entries)
       |> Map.new(fn entry ->
         {
           SpaceEx.Types.decode(entry.key, unquote(key_type)),
@@ -67,88 +111,12 @@ defmodule SpaceEx.Types do
     end
   end
 
-  def decode_ast(input, %{"code" => "DICTIONARY_ENTRY", "types" => [subtype]}) do
-    quote location: :keep do
-      SpaceEx.Protobufs.Dictionary.decode(unquote(input)).entries
-      |> Map.new(fn item ->
-        SpaceEx.Types.decode(item, unquote(subtype))
-      end)
-    end
+  def processor(:post_decode, code, types, _opts) when is_list(types) do
+    raise "Unknown type code with subtypes: #{inspect(code)}"
   end
 
-  def decode_ast(input, %{"code" => "CLASS", "service" => _service, "name" => _class}) do
-    input
-  end
-
-  def decode_ast(input, %{"code" => "STRING"}) do
-    <<first_byte, _ :: binary>> =
-      Raw.String.new(value: "dummy")
-      |> Raw.String.encode
-
-    quote do
-      Raw.String.decode(<<unquote(first_byte)>> <> unquote(input)).value
-    end
-    #quote do
-    #  {size, str} = :gpb.decode_varint(unquote(input))
-    #  <<_str :: bytes-size(size)>> = str
-    #end
-  end
-
-  def decode_ast(input, %{"code" => "FLOAT"}) do
-    <<first_byte, _ :: binary>> =
-      Raw.Float.new(value: 123.4)
-      |> Raw.Float.encode
-
-    quote do
-      Raw.Float.decode(<<unquote(first_byte)>> <> unquote(input)).value
-    end
-  end
-
-  def decode_ast(input, %{"code" => "DOUBLE"}) do
-    <<first_byte, _ :: binary>> =
-      Raw.Double.new(value: 123.4)
-      |> Raw.Double.encode
-
-    quote do
-      Raw.Double.decode(<<unquote(first_byte)>> <> unquote(input)).value
-    end
-  end
-
-  def decode_ast(input, %{"code" => "SINT32"}) do
-    <<first_byte, _ :: binary>> =
-      Raw.SInt32.new(value: 123)
-      |> Raw.SInt32.encode
-
-    quote do
-      Raw.SInt32.decode(<<unquote(first_byte)>> <> unquote(input)).value
-    end
-  end
-
-  def decode_ast(input, %{"code" => "BOOL"}) do
-    quote do
-      case unquote(input) do
-        <<0>> -> false
-        <<1>> -> true
-      end
-    end
-  end
-
-  def decode_ast(input, %{"code" => "BYTES"}), do: input
-
-  def decode_ast(input, %{"code" => code} = opts) do
-    if !Map.has_key?(opts, "types") do
-      decode_protobuf_ast(input, code)
-    else
-      nil
-    end || decode_unknown_ast(code)
-  end
-
-  def decode_unknown_ast(code) do
-    IO.puts "Unknown type code: #{code}"
-    quote do
-      raise "Unknown type code: #{unquote(code)}"
-    end
-  end
+  def processor(:post_decode, _, _, _), do: nil
+  def processor(:pre_encode,  _, _, _), do: nil
 
   SpaceEx.Protobufs.defs
   |> Enum.map(&elem(&1, 0))
@@ -159,14 +127,16 @@ defmodule SpaceEx.Types do
       |> SpaceEx.Service.module_basename
       |> SpaceEx.Service.to_snake_case
       |> String.upcase
+      |> String.to_atom
 
-    def decode_protobuf_ast(input, unquote(type_code)) do
-      module = unquote(module)
-      quote do
-        unquote(module).decode(unquote(input))
-      end
-    end
+    def protobuf_module(unquote(type_code)), do: unquote(module)
   end)
 
-  def decode_protobuf_ast(_, _), do: nil
+  def protobuf_module(_), do: nil
+
+  def decode_tuple(items, decoders) do
+    Enum.zip(items, decoders)
+    |> Enum.map(fn {item, decoder} -> decoder.(item) end)
+    |> List.to_tuple
+  end
 end
