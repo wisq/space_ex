c7994617ead72ed336c4b12524480c52d0bf0be1
Adrian Irving-Beer <wisq@wisq.net>
2018-01-13 19:50:24 -0500

    Make PipeliningConnection the standard Connection.

--- lib/space_ex/connection.ex
+++ lib/space_ex/connection.ex
@@ -4,12 +4,25 @@ defmodule SpaceEx.Connection do
     ConnectionRequest,
     ConnectionResponse,
     ProcedureCall,
-    Argument,
     Request,
     Response,
   }
 
+  defmodule State do
+    @enforce_keys [:socket, :reply_queue]
+    defstruct(
+      socket: nil,
+      reply_queue: nil,
+      buffer: <<>>,
+    )
+  end
+
   def connect!(host, port) do
+    {:ok, pid} = GenServer.start_link(__MODULE__, [host, port])
+    pid
+  end
+    
+  def init([host, port]) do
     sock = Socket.TCP.connect!(host, port, packet: :raw)
 
     request =
@@ -27,8 +40,11 @@ defmodule SpaceEx.Connection do
       _ -> raise "kRPC connection failed: #{inspect response.message}"
     end
 
-    {:ok, pid} = GenServer.start_link(__MODULE__, sock)
-    pid
+    Socket.active(sock)
+    {:ok, %State{
+      socket: sock,
+      reply_queue: :queue.new,
+    }}
   end
 
   def send_message(sock, message) do
@@ -39,6 +55,9 @@ defmodule SpaceEx.Connection do
     Socket.Stream.send!(sock, size <> message)
   end
 
+  # Only called during initialisation.
+  # After that, the socket is in active mode,
+  # and all replies come via handle_info messages.
   def recv_message(sock, buffer \\ <<>>) do
     case Socket.Stream.recv!(sock, 1) do
       <<1 :: size(1), _ :: bitstring>> = byte ->
@@ -53,7 +72,7 @@ defmodule SpaceEx.Connection do
     end
   end
 
-  def call_rpc(pid, service, procedure, args \\ []) do
+  def call_rpc(pid, service, procedure, args) do
     args =
       Enum.with_index(args)
       |> Enum.map(fn {arg, index} ->
@@ -64,7 +83,7 @@ defmodule SpaceEx.Connection do
       service: service,
       procedure: procedure,
       arguments: args,
-    ) |> IO.inspect
+    )
 
     request =
       Request.new(calls: [call])
@@ -73,7 +92,6 @@ defmodule SpaceEx.Connection do
     response =
       GenServer.call(pid, {:rpc, request})
       |> Response.decode
-      |> IO.inspect
 
     if response.error do
       {:error, response.error}
@@ -88,8 +106,62 @@ defmodule SpaceEx.Connection do
     end
   end
 
-  def handle_call({:rpc, bytes}, _, sock) do
-    send_message(sock, bytes)
-    {:reply, recv_message(sock), sock}
+  def handle_call({:rpc, bytes}, from, state) do
+    send_message(state.socket, bytes)
+    queue = :queue.in(from, state.reply_queue)
+
+    {:noreply, %State{state | reply_queue: queue}}
   end
+
+  def handle_info({:tcp, sock, bytes}, %State{socket: sock} = state) do
+    buffer = state.buffer <> bytes
+    {queue, buffer} = dispatch_replies(state.reply_queue, buffer)
+
+    {:noreply, %State{state |
+      reply_queue: queue,
+      buffer: buffer,
+    }}
+  end
+
+  def dispatch_replies(queue, buffer) do
+    case extract_reply(buffer) do
+      {:ok, reply, new_buffer} ->
+        new_queue = dispatch_reply(queue, reply)
+        dispatch_replies(new_queue, new_buffer)
+
+      {:error, :incomplete} -> {queue, buffer}
+    end
+  end
+
+  def dispatch_reply(queue, reply) do
+    {{:value, from}, queue} = :queue.out(queue)
+    GenServer.reply(from, reply)
+    queue
+  end
+
+  def extract_reply(buffer) do
+    case safe_decode_varint(buffer) do
+      {size, leftover} ->
+        case leftover do
+          <<reply :: bytes-size(size), buffer :: binary>> ->
+            {:ok, reply, buffer}
+
+          _ -> {:error, :incomplete}
+        end
+
+      nil -> {:error, :incomplete}
+    end
+  end
+
+  def safe_decode_varint(bytes) do
+    if has_varint?(bytes) do
+      :gpb.decode_varint(bytes)
+    else
+      nil
+    end
+  end
+
+  def has_varint?(<<>>), do: false
+  def has_varint?(<<0 :: size(1), _ :: bitstring>>), do: true # high bit unset, varint complete
+  def has_varint?(<<1 :: size(1), _ :: size(7), rest :: bitstring>>), do: has_varint?(rest) # high bit set, varint incomplete
 end
