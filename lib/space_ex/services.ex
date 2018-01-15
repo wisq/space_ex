147899acf6c43b92010b6670e3d1b5aa5534ed49
Adrian Irving-Beer <wisq@wisq.net>
2018-01-15 16:39:51 -0500

    Remove ensure_compiled check, causing issues with removed krpc.ex.

--- lib/space_ex/services.ex
+++ lib/space_ex/services.ex
@@ -1,11 +1,4 @@
 require SpaceEx.Gen
 
 SpaceEx.API.service_names
-|> Enum.each(fn name ->
-  module = Module.concat(SpaceEx, name)
-
-  case Code.ensure_compiled(module) do
-    {:module, ^module} -> :ok
-    {:error, :nofile} -> SpaceEx.Gen.generate_service(name)
-  end
-end)
+|> Enum.each(&SpaceEx.Gen.generate_service(&1))
