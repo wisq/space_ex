bb3d66c51cff896e7df043299dd541d66a0d145e
Adrian Irving-Beer <wisq@wisq.net>
2018-01-15 19:08:33 -0500

    Rename and document stream/procedure funcs.

--- lib/space_ex/procedure.ex
+++ lib/space_ex/procedure.ex
@@ -1,30 +1,77 @@
 defmodule SpaceEx.Procedure do
-  @enforce_keys [:module, :function, :rpc_service, :rpc_method, :rpc_args]
+  @moduledoc """
+  Represents a remote procedure call, for streams and expressions.
+
+  Normally created using the `create/4` function or the `call/1` macro.
+  """
+
+  @enforce_keys [:module, :function, :conn, :rpc_service, :rpc_method, :rpc_args]
   defstruct(
     module: nil,
     function: nil,
+    conn: nil,
     rpc_service: nil,
     rpc_method: nil,
     rpc_args: [],
   )
 
-  defmacro call({{:., _, [{:__aliases__, _, mod_parts}, func]}, _, args}) do
-    quote do
-      module   = Module.concat(unquote(mod_parts))
-      function = unquote(func)
-      [_conn | args] = unquote(args)
+  @doc """
+  Creates a procedure structure, representing a kRPC procedure call.
 
-      rpc_service = module.rpc_service_name()
-      rpc_method  = module.rpc_method_name(function)
-      rpc_args    = module.rpc_encode_arguments(function, args)
+  * `conn` — a `SpaceEx.Connection`
+  * `module` — a SpaceEx kRPC module, e.g. `SpaceEx.SpaceCenter.Vessel`
+  * `function` — the function name, as an atom, e.g. `:get_max_thrust`
+  * `args` — the arguments as an array (if any)
 
-      %SpaceEx.Procedure{
-        module: module,
-        function: function,
-        rpc_service: rpc_service,
-        rpc_method: rpc_method,
-        rpc_args: rpc_args,
-      }
+  Any arguments will be immediately encoded, so most errors related to argument
+  data (e.g. incorrect type) will be detected immediately.
+  """
+
+  def new(conn, module, function, args \\ []) do
+    rpc_service = module.rpc_service_name()
+    rpc_method  = module.rpc_method_name(function)
+    rpc_args    = module.rpc_encode_arguments(function, args)
+
+    %SpaceEx.Procedure{
+      module: module,
+      function: function,
+      conn: conn,
+      rpc_service: rpc_service,
+      rpc_method: rpc_method,
+      rpc_args: rpc_args,
+    }
+  end
+
+  @doc """
+  Creates a procedure structure based on an actual procedure call.
+
+  You can wrap any normal API function call in this, and it will parse that
+  into a format suitable for using in streams, expressions, etc.  For example:
+
+  ```elixir
+  require SpaceEx.Procedure
+
+  call1 = SpaceEx.Procedure.create(SpaceEx.SpaceCenter.get_ut(conn))
+
+  # You can also use pipelining:
+  call2 =
+    SpaceEx.SpaceCenter.Flight.get_mean_altitude(conn, flight)
+    |> SpaceEx.Procedure.create
+  ```
+
+  `SpaceEx.Procedure.create(Mod.func(conn, args))` is equivalent to calling
+  `SpaceEx.Procedure.new(conn, Mod, :func, args)`.
+  """
+
+  defmacro create({{:., _, [module, func]}, _, args}) do
+    quote bind_quoted: [
+      module: module,
+      function: func,
+      macro_args: args
+    ] do
+      [conn | args] = macro_args
+
+      SpaceEx.Procedure.new(conn, module, function, args)
     end
   end
 end
--- lib/space_ex/stream.ex
+++ lib/space_ex/stream.ex
@@ -2,6 +2,54 @@ defmodule SpaceEx.Stream do
   use GenServer
   alias SpaceEx.Stream
 
+  @moduledoc """
+  Enables data to be streamed as it changes, instead of polled repeatedly.
+
+  Streams are an efficient way to repeatedly retrieve data from the game.
+
+  To set up a stream, you can use `SpaceEx.Stream.stream/2`.
+
+  Alternatively, you can create `SpaceEx.Procedure` that references the
+  procedure you want to run, and then pass that to `SpaceEx.Stream.create`.
+
+  In most situations where you find yourself asking for the same piece of data
+  over and over, you're probably better off using a stream.  This will reduce
+  the load on both ends and make your code run faster.
+
+  Example usage:
+
+  ```elixir
+  require SpaceEx.Procedure
+  stream =
+    SpaceEx.SpaceCenter.get_ut(conn)
+    |> SpaceEx.Procedure.call
+    |> SpaceEx.Stream.create(start: true)
+
+  SpaceEx.Stream.get(stream)  # 83689.09043863538
+  Process.sleep(100)
+  SpaceEx.Stream.get(stream)  # 83689.1904386354
+  Process.sleep(100)
+  SpaceEx.Stream.get(stream)  # 83689.29043863542
+
+  # You can also create a nifty "magic variable" shorthand:
+  ut = SpaceEx.Stream.get_fn(stream)
+
+  ut.()  # 83689.29043863544
+  Process.sleep(100)
+  ut.()  # 83689.39043863546
+  Process.sleep(100)
+  ut.()  # 83689.49043863548
+
+  # You can even create both the stream and the shortcut at once:
+  {stream, ut} =
+    SpaceEx.SpaceCenter.get_ut(conn)
+    |> SpaceEx.Stream.stream
+
+  SpaceEx.Stream.get(stream)  # 83689.09043863540
+  ut.()  # 83689.49043863541
+  ```
+  """
+
   defmodule State do
     @moduledoc false
 
@@ -13,14 +61,29 @@ defmodule SpaceEx.Stream do
     )
   end
 
-  @enforce_keys [:pid, :decoder]
+  @enforce_keys [:id, :pid, :decoder]
   defstruct(
+    id: nil,
     pid: nil,
     decoder: nil,
-    getter: nil,
   )
 
-  def create(conn, procedure, opts \\ []) do
+  @doc """
+  Creates a stream, and optionally starts it.
+
+  `procedure` should be a `SpaceEx.Procedure` structure generated using
+  `SpaceEx.Procedure.call/1`.  The stream's value will be the result of calling
+  this function over and over (with the same arguments each time).
+
+  See the module documentation for usage examples.
+
+  ## Options
+
+  * `:start` — when `false`, the stream is created, but not started.  The default is `start: true`.
+  """
+
+  def create(procedure, opts \\ []) do
+    conn = procedure.conn
     start = opts[:start] || true
 
     {:ok, stream_obj} = SpaceEx.KRPC.add_stream(conn, procedure, start)
@@ -32,23 +95,99 @@ defmodule SpaceEx.Stream do
     decoder = fn value ->
       procedure.module.rpc_decode_return_value(procedure.function, value)
     end
-    stream = %Stream{pid: pid, decoder: decoder}
-
-    getter = fn -> Stream.get_value(stream) end
-    %Stream{stream | getter: getter}
+    %Stream{id: stream_id, pid: pid, decoder: decoder}
   end
 
-  def start_link(stream_id) do
-    GenServer.start_link(__MODULE__, %State{id: stream_id})
+  @doc """
+  Creates a stream directly from function call syntax.
+
+  This is equivalent to calling `SpaceEx.Procedure.create/1` and `SpaceEx.Stream.create/2`.
+
+  ## Example
+
+  ```elixir
+  stream =
+    SpaceEx.SpaceCenter.Flight.get_mean_altitude(conn, flight)
+    |> SpaceEx.Stream.create
+
+  SpaceEx.Stream.get(stream)  # 76.64177794696297
+  ```
+  """
+
+  defmacro stream(function_call, opts) do
+    quote do
+      SpaceEx.Procedure.create(unquote(function_call))
+      |> SpaceEx.Stream.new(unquote(opts))
+    end
   end
+
+  @doc """
+  Creates a stream and query function directly from function call syntax.
+
+  This is equivalent to calling `SpaceEx.Procedure.create/1`,
+  `SpaceEx.Stream.create/2`, and `SpaceEx.Stream.get_fn/1`, all in sequence.
+
+  Returns a tuple containing the stream and the getter function.
+
+  ## Example
+
+  ```elixir
+  {stream, altitude} =
+    SpaceEx.SpaceCenter.Flight.get_mean_altitude(conn, flight)
+    |> SpaceEx.Stream.stream_fn
+
+  altitude.() |> IO.inspect  # 76.64177794696297
+  ```
+  """
+
+  defmacro stream_fn(function_call, opts) do
+    quote do
+      stream = SpaceEx.Stream.stream(unquote(function_call), unquote(opts))
+
+      {stream, SpaceEx.Stream.get_fn(stream)}
+    end
+  end
+
+  @doc """
+  Returns an anonymous function that can be used to query the stream.
+
+  This function can make code cleaner, by emulating a sort of "magic variable"
+  that constantly updates itself to the current value.  For example, if you
+  assign `apoapsis = SpaceEx.Stream.get_fn(apo_stream)`, you can now use
+  `apoapsis.()` to get the up-to-date value at any time.
+  """
+
+  def get_fn(stream) do
+    fn -> get(stream) end
+  end
+
+  @doc """
+  Get (and decode) the current value from a stream.
+
+  This will retrieve the latest stream value and decode it.  (Because streams
+  can receive hundreds of updates every second, stream values are not decoded
+  until requested.)
+
+  Note that if a stream has not received any data yet, this function will block
+  for up to `timeout` milliseconds (default: 5 seconds) for the first
+  value to arrive.
+
+  A timeout usually indicates that the stream was created with `start: false`,
+  and it was not subsequently started before the timeout expired.
+  """
     
-  def get_value(stream) do
-    result = GenServer.call(stream.pid, :get)
+  def get(stream, timeout \\ 5000) do
+    result = GenServer.call(stream.pid, :get, timeout)
     if result.error do
       raise result.error
     else
       stream.decoder.(result.value)
     end
+  end
+
+  @doc false
+  def start_link(stream_id) do
+    GenServer.start_link(__MODULE__, %State{id: stream_id})
   end
 
   # If stream has no data yet, add caller to waitlist.
